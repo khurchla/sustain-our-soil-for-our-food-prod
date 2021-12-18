548443221725858ee573075fe7892bada28443d5
Kathryn Hurchla <kathryn@dadeda.design>
2021-12-18 16:04:30 -0500

    added spaces around server line in app|format free requirements.txt

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MORPH],
                             'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5'}]
                 )
 
-server=app.server
+server = app.server
 app.title='Sustain-Our-Soil-for-Our-Food'
 
 # ----------------------------------------------------------------------------------------
